525410424a65d3a5ee3c4dcd2984c64b22d6b712
luciano <lazaluciano327@gmail.com>
2025-03-11 15:56:24 +0300

    Project card

---
+++ app/action.ts
@@ -0,0 +1,97 @@
+"use server";
+
+import prisma from "@/lib/lib";
+import { randomBytes } from "crypto";
+
+export async function checkAndAddUser(email: string, name: string) {
+  if (!email) return;
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (!existingUser && name) {
+      await prisma.user.create({
+        data: {
+          email: email,
+          name: name,
+        },
+      });
+      console.error("Erreur lors de la verification de l'utilisateur");
+    } else {
+      console.error("Utilisateur déjà présent dans la base de données");
+    }
+  } catch (error) {
+    console.error("Erreur lors de la verification de l'utilisateur", error);
+  }
+}
+
+function generateUniqueCode(): string {
+  return randomBytes(6).toString("hex");
+}
+
+export async function createProject(
+  name: string,
+  description: string,
+  email: string
+) {
+  if (!name || !description || !email) return;
+  try {
+    const inviteCode = generateUniqueCode();
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (!user) {
+      throw new Error("Utilisateur non trouvé");
+    }
+
+    const newProject = await prisma.project.create({
+      data: { name, description, inviteCode, createdById: user.id },
+    });
+
+    return newProject;
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+}
+export async function getProjectsCreatedByUser(email: string) {
+  if (!email) return;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (!user) {
+      throw new Error("Utilisateur non trouvé");
+    }
+    const projects = await prisma.project.findMany({
+      where: { createdById: user.id },
+      include: {
+        tasks: {
+          include: {
+            user: true,
+            createdBy: true,
+          },
+        },
+        users: {
+          select: {
+            user: {
+              select: {
+                id: true,
+                name: true,
+                email: true,
+              },
+            },
+          },
+        },
+      },
+    });
+    const formatedProjects = projects.map((project) => ({
+      ...project,
+      users: project.users.map((userEntry) => userEntry.user),
+    }));
+    return formatedProjects;
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+}
---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,140 @@
+import React, { FC } from "react";
+import { Project } from "@/type";
+import { Copy, ExternalLink, FolderGit2, Trash } from "lucide-react";
+import Link from "next/link";
+
+interface ProjectProps {
+  project: Project;
+  admin: number;
+  style: boolean;
+}
+
+const ProjectCard: FC<ProjectProps> = ({ project, admin, style }) => {
+  const totalTasks = project.tasks?.length;
+  const tasksByStatus = project.tasks?.reduce(
+    (acc, task) => {
+      if (task.status === "To Do") acc.toDo++;
+      else if (task.status === "In Progress") acc.inProgress++;
+      else if (task.status === "Done") acc.done++;
+      return acc;
+    },
+    {
+      toDo: 0,
+      inProgress: 0,
+      done: 0,
+    }
+  ) ?? { toDo: 0, inProgress: 0, done: 0 };
+  const progressPecentage = totalTasks
+    ? Math.round((tasksByStatus.done / totalTasks) * 100)
+    : 0;
+  const inProgressPercentage = totalTasks
+    ? Math.round((tasksByStatus.inProgress / totalTasks) * 100)
+    : 0;
+  const toDoPercentage = totalTasks
+    ? Math.round((tasksByStatus.toDo / totalTasks) * 100)
+    : 0;
+  const textSizeClass = style ? "text-sm" : "text-md";
+  return (
+    <div
+      className={`${
+        style ? `border border-base-300 p-5 shadow-sm` : ``
+      }  text-base-content rounded-xl w-full text-left`}
+      key={project.id}
+    >
+      <div className="w-full flex items-center mb-3">
+        <div className="bg-primary-content text-xl h-10 w-10 rounded-lg flex justify-center">
+          <FolderGit2 className="w-6 text-primary" />
+        </div>
+        <div className="badge m-3 font-bold">{project.name}</div>
+      </div>
+      {style == false && (
+        <p className="text-sm text-gray-500 border border-base-300 p-5 mb-6 rounded-xl">
+          {project.description}
+        </p>
+      )}
+      <div className={`mb-3 `}>
+        <span>Collaborateurs</span>
+        <div className="badge badge-sm ml-1 badge-ghost">
+          {project.users?.length}
+        </div>
+      </div>
+      {admin === 1 && (
+        <div className="flex justify-between items-center rounded-lg p-2 border border-base-300 mb-3 bg-base-200/30">
+          <p className="text-primary font-bold m-3">{project.inviteCode}</p>
+          <button className="btn btn-sm ml-2">
+            <Copy className="w-4" />
+          </button>
+        </div>
+      )}
+      <div className="flex flex-col mb-3">
+        <h2 className={`text-gray-500 mb-2`}>
+          <span className="font-bold">A faire</span>
+          <div className="badge badge-ghost badge-sm ml-1">
+            {tasksByStatus.toDo}
+          </div>
+        </h2>
+        <progress
+          className="progress progress-primary w-full"
+          value={toDoPercentage}
+          max="100"
+        ></progress>
+        <div className="flex">
+          <span className={`text-gray-400 mt-2 `}>{toDoPercentage}%</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col mb-3">
+        <h2 className={`text-gray-500 mb-2`}>
+          <span className="font-bold">En cours</span>
+          <div className="badge badge-ghost badge-sm ml-1">
+            {tasksByStatus.inProgress}
+          </div>
+        </h2>
+        <progress
+          className="progress progress-primary w-full"
+          value={inProgressPercentage}
+          max="100"
+        ></progress>
+        <div className="flex">
+          <span className={`text-gray-400 mt-2 `}>{inProgressPercentage}%</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col mb-3">
+        <h2 className={`text-gray-500 mb-2`}>
+          <span className="font-bold">Terminée(s)</span>
+          <div className="badge badge-ghost badge-sm ml-1">
+            {tasksByStatus.done}
+          </div>
+        </h2>
+        <progress
+          className="progress progress-primary w-full"
+          value={progressPecentage}
+          max="100"
+        ></progress>
+        <div className="flex">
+          <span className={`text-gray-400 mt-2 `}>{progressPecentage}%</span>
+        </div>
+      </div>
+      <div className="flex">
+        {style && (
+          <Link
+            className="btn btn-primary btn-sm"
+            href={`/projet/${project.id}`}
+          >
+            <div className="badge badge-sm">{totalTasks}</div>
+            Tâche
+            <ExternalLink className="w-4" />
+          </Link>
+        )}
+        {admin === 1 && (
+          <button className="btn btn-sm ml-3 btn-secondary">
+            <Trash className="w-4" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
